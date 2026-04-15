dd036e24965a07eae8e9f6ef37a81c81ae21e27f
Ouhfi <ouhfid4@gmail.com>
2026-04-16 00:08:21 +0100

    script test

--- script/script.js
+++ script/script.js
@@ -1,4 +1,21 @@
-const input = document.getElementById("num");
-const btn = document.getElementById("btn");
-const result = document.getElementById("result");
 
+const api = "https://api.tvmaze.com/shows";
+
+async function getdata(){
+
+
+    let result =  await fetch(api);
+    if(!result) return;
+
+    let jsondata = await result.json();
+
+    for(let i of jsondata.slice(0 , 6)){
+
+
+    }
+        
+    
+
+
+    
+} 
